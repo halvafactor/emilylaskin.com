e1a9acfd591fdb66a59295fd4563342461c1369e
halvafactor <emily.laskin@gmail.com>
2026-02-18 14:15:09 -0800

    Change homepage description metadata to wordcel at large

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "Emily Laskin",
 	url: "https://emilylaskin.com",
 	language: "en",
-	description: "does words things",
+	description: "wordcel at large",
 	author: {
 		name: "Emily Laskin",
 		email: "emily.laskin@gmail.com",
